7f06a6a9965f098bedab79a4ff9835abafcdb766
Radiant <you@example.com>
2020-09-18 21:46:25 +0700

    menambah dua files

---
+++ script.js
@@ -0,0 +1,19 @@
+const asalSantri = {
+    sidik: "Lampung",
+    wandi: "Prabumulih",
+    rahmad: "Medan",
+    usamah: "Ceribon"
+};
+({
+    sidik,
+    wandi,
+    rahmad,
+    usamah,
+} = asalSantri);
+console.log(sidik, wandi, rahmad, usamah);
+const asalSantri2 = {
+    "Syofyan": "Purwokerto",
+    "Aldi": "Kendari",
+    "Kardi": "Lombok"
+};
+console.log(asalSantri2)
